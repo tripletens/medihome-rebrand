c8aaff29f33b268d2b1c85a494fe4279f571e2cd
xtreemtech <visitmrdaniel@gmail.com>
2024-01-09 22:08:01 +0100

    Update about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,83 @@
 <x-landing-layout>
 
     <x-shortbanner title="About">
-        
     </x-shortbanner>
+    
+    <div class="grid grid-rows-3 grid-flow-col gap-4 mt-6">
+
+  <div class="row-span-3 mt-6">
+
+  <div class="mx-auto max-w-2xl lg:text-center">
+      <p class=" text-left mt-6 text-base leading-8 font-semibold text-slate-500">MEDIHOME Care Services are passionate about delivering quality services of an extremely high standard as if it were our loved ones receiving care and support. Allow our multidisciplinary Medihome team provide in-house services for your loved ones. During the Coronavirus pandemic, especially the lockdowns, we were able to provide outstanding quality care for our customers in accordance with covid protocol, both existing and new. We ensured staff are fully protected against the Coronavirus, with personal protective clothing, sanitizer, weekly covid tests, training, policies ... No Wonder MEDIHOME is recognised as one of the best and top home care providers. We have a proven track records, reputation and are rated as consistent by Homecare service providers in Nigeria.</p>
+
+ <h1 class="mt-6 font-bold text-lg ">MEDIHOME TEAM & STAFF STRENGTH</h1>
+<h3>Our medical team majorly consist of dedicated professionals:</h3>
+
+<div class="grid gap-1 grid-cols-3 ">
+<a href="#" class="group block max-w-md mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 mt-3">
+  <div class="flex items-center space-x-3">
+    <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">High rank medical Consultants from different specialties</h3>
+  </div>
+</a>
+<a href="#" class="group block max-w-md mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 mt-3">
+  <div class="flex items-center space-x-3">
+    <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">Specialist Nurses</h3>
+  </div>
+</a>
+<a href="#" class="group block max-w-md mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 mt-3">
+  <div class="flex items-center space-x-3">
+    <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">Physical therapists</h3>
+  </div>
+</a>
+<a href="#" class="group block max-w-md mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 mt-3">
+  <div class="flex items-center space-x-3">
+    <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">Laboratory scientists</h3>
+  </div>
+</a>
+<a href="#" class="group block max-w-md mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 mt-3">
+  <div class="flex items-center space-x-3">
+    <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">Clinical Pharmacists</h3>
+  </div>
+</a>
+<a href="#" class="group block max-w-md mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 mt-3">
+  <div class="flex items-center space-x-3">
+    <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">Dieticians</h3>
+  </div>
+</a>
+<a href="#" class="group block max-w-md mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 mt-3">
+  <div class="flex items-center space-x-3">
+    <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">Professional Caregivers</h3>
+  </div>
+</a>
+
+</div>
+    </div>
+  </div>
+  
+
+  <a href="#" class="group block max-w-md mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 mt-3">
+  <div class="flex items-center space-x-3">
+    
+    <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">OUR VISION</h3>
+  </div>
+  <p class="text-slate-500 group-hover:text-white text-sm">To become world class leader and preferred home care organization by continually evaluating improving the way we conduct ourselves, ensuring our patients always receive exceptional services and care</p>
+</a>
+<a href="#" class="group block max-w-md mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500">
+  <div class="flex items-center space-x-3">
+    
+    <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">OUR MISSION</h3>
+  </div>
+  <p class="text-slate-500 group-hover:text-white text-sm">To deliver high quality care to improve and maintain a healthy quality of life for all people while using a holistic person-centered care approach in the delivery of medical and non-medical home care, social services and supports.</p>
+</a>
+<a href="#" class="group block max-w-md mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500">
+  <div class="flex items-center space-x-3">
+    
+    <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">OUR VALUES</h3>
+  </div>
+  <p class="text-slate-500 group-hover:text-white text-sm">MediHome's standard of Excellence.</p>
+  <button class="bg-cyan-500 shadow-lg shadow-cyan-500/50 ...">Empathy</button>
+</a>
+</div>
     <div></div>
+    
 </x-landing-layout>
